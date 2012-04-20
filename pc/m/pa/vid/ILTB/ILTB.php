92eb0b0b6a719d169ed40ac6140503c34144e278
watchingthecloud <admin@watchingthecloud.com>
2012-04-20 12:24:10 +0100

    20/04/12 bugfix

---
+++ pc/m/pa/vid/ILTB/ILTB.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../../../style.css">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script src="scripts/yui-min.js"></script>
+	<!--apple meta-->
+		<meta name="viewport" content="width = device-width, initial-scale = 1.0, user-scalable = no" />
+		<link rel="apple-touch-icon" href="../../../images/ICON.png">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<link rel="apple-touch-startup-image" href="../../../images/SPLASH.png">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<!--apple meta-->
+<title>Unconscious injured</title>
+	</head>
+	<body>
+		<div id="header">
+			<img align="left" id="headflag" src="../../../images/sprite.png"/>
+			<img align="right" id="logo" src="../../../images/logohead.png"/>
+			Unconscious injured
+		</div>
+		<div id="content">
+			<!------------------------------------------------------------------------------------------------->
+			<div id="viewlink"><a href="javascript:history.go(-1)" data-direction="reverse" class="backbtn">Back</a></div> 
+			<div id="video">
+				<video  width="300px"  src="ILTB.mp4" controls></video>
+			</div>
+			<!------------------------------------------------------------------------------------------------->
+		</div>		
+		<div id="menu" class="menu">
+			<ul>
+				<a href="../../../mcq.html"><li id="mcqbutton" class="menubutton" ><img src="../../../images/quizicon_sml.png"/></li></a>
+				<a href="../../../triage.html"><li id="triagebutton" class="menubutton"><img src="../../../images/triageicon_sml.png"/></li></a>
+				<a href="../../pa-home.html"><li id="patbutton" class="menubutton" ><img src="../../../images/assessmenticon_sml.png"/></li></a>
+				<a href="../../../kit/kit-home.html"><li id="kitbutton" class="menubutton" ><img src="../../../images/bagicon_sml.png"/></li></a>
+				<a href="../../../numbers.html"><li id="numbersbutton" class="menubutton" ><img src="../../../images/infoicon_sml.png"/></li></a>
+			</ul>
+		</div>
+	</body>
+</html>
+
+<!--------------------------------------------------------------------------------------------------------------------------------->
